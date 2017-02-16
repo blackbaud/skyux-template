06c89c0712bc11ab1f530809dace9c298b1bf07b
Bobby Earl <bobby.earl@blackbaud.com>
2017-02-16 09:02:53 -0500

    Cleaned up e2e tests

--- e2e/home.component.e2e-spec.ts
+++ e2e/home.component.e2e-spec.ts
@@ -1,13 +1,22 @@
-import { browser, ExpectedConditions } from 'protractor';
-import { SkyHostBrowser } from '@blackbaud/skyux-builder/runtime';
+// Use browser to access other sites (that are running angular)
+import { element, by } from 'protractor';
+
+// Use SkyHostBrowser to access your locally served SPA
+import { SkyHostBrowser } from '@blackbaud/skyux-builder/runtime/testing';
 
 describe('Home Component', () => {
+
+  // done is optional callback if you're doing async tests
   it('should render', (done) => {
-    SkyHostBrowser.get('/');
+
+    // Change to your SPA's name
+    SkyHostBrowser.get('/rrrrr-app-name');
     expect(element(by.tagName('h1')).getText()).toBe('SKY UX Template');
     expect(element(by.className('sky-alert')).getText()).toBe(
       `You've just taken your first step into a larger world.`
     );
+
+    // Since we've said this is an async test, we need to call done
     done();
   });
 });
