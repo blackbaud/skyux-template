39deaf6e0aba97fb85927a29b3f08559b34e58e9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-06-20 12:17:03 -0400

    Exporting sky module

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule
+} from '@angular/core';
+
+require('style-loader!@blackbaud/skyux/dist/css/sky.css');
+
+import {
+  SkyModule
+} from '@blackbaud/skyux/dist/core';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
+  exports: [
+    SkyModule
+  ],
   providers: [],
   entryComponents: []
 })
