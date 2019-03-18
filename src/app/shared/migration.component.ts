7d23f5deb025ef2a8ac92cf26368670ef00c2e60
Bobby Earl <bobby.earl@blackbaud.com>
2019-03-18 14:40:51 -0400

    Added migration component. (#61)
    Added migration component.

---
+++ src/app/shared/migration.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-migration',
+  templateUrl: './migration.component.html',
+  styleUrls: ['./migration.component.scss']
+})
+export class AppMigrationComponent {}
